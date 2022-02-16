d091339c2c3e486284283b3544f5ec1295de5f70
shahbazalis <engr.shahbaz92@gmail.com>
2022-02-16 12:35:19 +0200

    initial heat map calendar implementation

--- src/views/heatMapCalendar/heatMapCalendar.tsx
+++ src/views/heatMapCalendar/heatMapCalendar.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import CalendarHeatmap from "react-calendar-heatmap";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
 const HeatMapCalendar = () => {
-  return <h1>Heat Map Calendar</h1>;
+  const today = new Date();
+
+  return (
+    <div>
+      <Paper>
+        <h1>Heat Map Calendar</h1>
+        <Grid container spacing={2}>
+          <CalendarHeatmap
+            startDate={new Date("2021-01-01")}
+            endDate={today}
+            gutterSize={5}
+            showWeekdayLabels={true}
+            values={[
+              { date: "2022-01-01" },
+              { date: "2022-01-22" },
+              { date: "2022-01-30" },
+            ]}
+          />
+        </Grid>
+      </Paper>
+    </div>
+  );
 };
 
 export default HeatMapCalendar;
