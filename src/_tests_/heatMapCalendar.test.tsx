041b3d5d9e59596c7262ff55b626251007cb97b5
shahbazalis <engr.shahbaz92@gmail.com>
2022-02-27 17:06:50 +0200

    test cases updated

--- src/_tests_/heatMapCalendar.test.tsx
+++ src/_tests_/heatMapCalendar.test.tsx
@@ -1,15 +1,23 @@
-import { render, RenderResult } from "@testing-library/react";
-import Enzyme from "enzyme";
+//import { render, RenderResult } from "@testing-library/react";
+import Enzyme,{mount} from "enzyme";
 import HeatMapCalendar from "../views/heatMapCalendar/heatMapCalendar";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("Snapshot testing heatmapcalnder", () => {
-  it("snapshot heatmap calendar", async () => {
-    let wrapper: RenderResult;
-    wrapper = render(<HeatMapCalendar />);
-    expect(wrapper).toMatchSnapshot();
+// describe("Snapshot testing heatmapcalnder", () => {
+//   it("snapshot heatmap calendar", async () => {
+//     let wrapper: RenderResult;
+//     wrapper = render(<HeatMapCalendar />);
+//     expect(wrapper).toMatchSnapshot();
+//   });
+// });
+
+describe("< HeatMapCalendar/>", () => {
+  it('should render as an svg', () => {
+    const wrapper = mount(< HeatMapCalendar/>);
+    expect(wrapper.find('svg')).toBeDefined();
+    expect(wrapper.find('svg')).toHaveLength(1);
   });
 });
